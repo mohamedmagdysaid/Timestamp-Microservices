43de1ac86932a0563c048af30f6179a880f976db
mohamedmagdysaid <mohamedmagdysaid@gmail.com>
2017-11-23 01:34:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,37 +4,83 @@
 // init project
 var express = require('express');
 var path  = require('path');
-var moment = require('moment');
+
 var fs = require('fs');
 var app = express();
-
-app.set('port',process.env.PORT ||3000);
+var port = process.env.PORT || 5000
+app.set('port',port);
 app.use(express.static(__dirname + '/views'))
 app.get('/',function(req,res){
   res.send('index',function(err){
     if (err) throw err;
   });
 });
-app.listen(process.env.PORT ||3000);
-app.get('/:datestring', function(req,res) {
-  var myDate;
-  if(/^\d{8,}$/.test(req.params.datestring)) {
-    myDate = moment(req.params.datestring, "X");
-  } else {
-    myDate = moment(req.params.datestring, "MMMM D, YYYY");
+app.route("/").get(function(req, res) {
+    res.sendFile(process.cwd() + '/public/index.html');
+  });
+
+  app.route("/:query").get(function(req, res) {
+    var query = req.params.query;
+
+    if (Number(query)) {
+      var unix = Number(query);
+      var formattedDate = formatDate(new Date(unix * 1000));
+    } else if (new Date(query)) {
+      var utcDate = createDateAsUTC(new Date(query));
+      var formattedDate = formatDate(utcDate);
+      var unix = utcDate.getTime() / 1000;
+    }
+
+    if (!unix || !formattedDate) {
+      unix = null;
+      formattedDate = null;
+    }
+
+    res.send({'unix': unix, 'natural': formattedDate});
+  });
+
+
+function formatDate(date) {
+  if (!(typeof date === 'object')) {
+    return;
   }
 
-  if(myDate.isValid()) {
-    res.json({
-      unix: myDate.format("X"),
-      natural: myDate.format("MMMM D, YYYY")
-    });
-  } else {
-    res.json({
-      unix: null,
-      natural: null
-    });
+  var month = monthIntToMonth(date.getUTCMonth());
+  var year = date.getUTCFullYear();
+  var date = date.getUTCDate();
+  return `${month} ${date}, ${year}`;
+}
+
+function monthIntToMonth(num) {
+  switch (num) {
+    case 0:
+      return 'January';
+    case 1:
+      return 'February';
+    case 2:
+      return 'March';
+    case 3:
+      return 'April';
+    case 4:
+      return 'May';
+    case 5:
+      return 'June';
+    case 6:
+      return 'July';
+    case 7:
+      return 'August';
+    case 8:
+      return 'September';
+    case 9:
+      return 'October';
+    case 10:
+      return 'November';
+    case 11:
+      return 'December';
   }
+};
 
-
-});
+function createDateAsUTC(date) {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()));
+}
+app.listen(port);
